d29bd2a3fbc8f8d9e5c8dca5a5b0fc5add355c61
Eylül Badem <eylul.badem@ug.bilkent.edu.tr>
2023-11-26 18:46:39 +0300

    myClasses base

--- routes/LearnerFeatures/myClasses.js
+++ routes/LearnerFeatures/myClasses.js
@@ -3,11 +3,38 @@ let router = express.Router()
 
 router
     .route("/addRequest")
-    .get((req, res) =>{
-
-        res.send("my calssjakhdas ")
-
-
+    .post((req, res) =>{
+        console.log(req.body)
+        console.log("myClasses-learner add request successful")
     })
 
-module.exports = router
+// show old classes for the learner
+router
+    .route("/showOldClasses")
+    .get((req, res) =>{
+        res.send("showing old classes")
+    })
+
+// show present classes for the learner
+router
+    .route("/showNewClasses")
+    .get((req, res) =>{
+        res.send("showing new classes")
+    })
+
+// show class detail
+router
+    .route("/classDetail")
+    .get((req, res) =>{
+        res.send("showing class detail")
+    })
+
+// send new class request
+router
+    .route("/newClassRequest")
+    .post((req, res) => {
+        console.log(req.body)
+    })
+
+
+module.exports = router 
