e34c6de39b874ed01ade7b2151f9546199c42a08
Eylül Badem <eylul.badem@ug.bilkent.edu.tr>
2023-12-04 14:50:12 +0300

    one step back on db.js

--- db.js
+++ db.js
@@ -1,31 +1,17 @@
+const { Client } = require('pg');
+const port = 6543;
 
-const {Client} = require('pg')
-const port = 6543
 const connection = new Client({
+   host: 'localhost',
+   port: port,
+   user: 'postgres',
+   password: 'supersecretpassword',
+   database: 'app',
+});
 
-    host: 'localhost',
-    port: port,
-    user: "postgres",
-    password: "supersecretpassword",
-    database: "app"
+connection.connect(function (err) {
+   if (err) throw err;
+   console.log('Connected');
+});
 
-})
-
-connection.connect(function(err) {
-    if(err) throw err;
-    console.log("Connected")
-})
-
-async function executeQuery(query) {
-    try {
-        const result = await connection.query(query);
-        return result.rows;
-    } catch (error) {
-        throw new Error(`Error executing query: ${error.message}`);
-    }
-}
-
-module.exports = {
-    connection,
-    executeQuery,
-};
+module.exports = connection;
