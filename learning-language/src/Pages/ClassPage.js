7dd766248859a6e58879345edaadff4c1ae6b6f3
Alperen Erdoğan <alperenerdogan@yahoo.com>
2023-12-12 18:38:56 +0300

    ClassPage Register

--- learning-language/src/Pages/ClassPage.js
+++ learning-language/src/Pages/ClassPage.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { Card, CardContent, Typography, Button } from '@mui/material';
+import { Card, CardContent, Typography, Button, Box, Grid, Paper } from '@mui/material';
 import CourseComponent from '../Components/CourseComponent';
 import Popup from '../Components/PopupComponent';
+import '../Css/Components.css'
 import MainLayout from '../Components/MainLayout';
 
 
 const activeCoursesData = [
     {id: 1, title: 'Sample Course', teacher: 'Alex Hale', language: 'English', level: 'A1', status: 'Active', progress: '%20', meetingDate: '01.01.2023', description: 'Description for the sample course.'},
-    {id: 2, title: 'Sample Course', teacher: 'Alex Hale', language: 'English', level: 'A1', status: 'Active', progress: '%20', meetingDate: '01.01.2023', description: 'Description for the sample course.'}
+    {id: 2, title: 'Sample Course', teacher: 'Alex Hale', language: 'English', level: 'A1', status: 'Active', progress: '%20', meetingDate: '01.01.2023', description: 'Description for the sample course.'},
+    {id: 5, title: 'Sample Course', teacher: 'Alex Hale', language: 'English', level: 'A1', status: 'Active', progress: '%20', meetingDate: '01.01.2023', description: 'Description for the sample course.'},
+    {id: 6, title: 'Sample Course', teacher: 'Alex Hale', language: 'English', level: 'A1', status: 'Active', progress: '%20', meetingDate: '01.01.2023', description: 'Description for the sample course.'}
 ];
 
 const finishedCoursesData = [
@@ -41,21 +44,40 @@ const ClassPage = () => {
     <MainLayout children={
       <div>
         <div>
+        <Paper>
           <Typography variant="h4" gutterBottom>
-            Active Courses
+            <Grid container >
+              <Grid item xs={6} className='margin-top-1'>
+              <Box sx={{ display: 'flex', justifyContent: 'flex-start', padding:1}}>
+                Active Courses
+              </Box>
+              </Grid>
+              <Grid item xs={6} className='margin-top-1'>
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end', padding:1}}>
+                <Button variant="contained" color="success">Register</Button>
+              </Box>
+              </Grid>
+            </Grid>
           </Typography>
-
+          <Box sx={{ display: 'flex', flexDirection: 'row', overflowX: 'auto', width:700 }}>
           {activeCourses.map((course) => (
             <CourseComponent course={course} />
           ))}
+          </Box>
+          </Paper>
         </div>
+
         <div>
+          <Paper>
           <Typography variant="h4" gutterBottom>
             Finished Courses
           </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', overflowX: 'auto', width:700 }}>
           {finishedCourses.map((course) => (
             <CourseComponent course={course}/>
           ))}
+          </Box>
+          </Paper>
         </div>
       </div>
     }/>
