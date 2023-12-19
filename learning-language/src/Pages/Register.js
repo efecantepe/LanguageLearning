d571c4d0dd653f3a1427b964d05c74a301ec8ac4
Alperen Erdoğan <alperenerdogan@yahoo.com>
2023-12-19 16:18:49 +0300

    Update Register.js

--- learning-language/src/Pages/Register.js
+++ learning-language/src/Pages/Register.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import urlList from '../urllist';
 import sendRequest from '../axios';
 import axios from 'axios';
+import { Container,Grid,TextField,FormControl,MenuItem,Select,InputLabel,Box,Button  } from '@mui/material';
+import '../Css/Components.css';
+import Header from '../Components/Header'
+
 
 const Register = () => {
   const [userName, setUserName] = useState('');
@@ -14,53 +18,50 @@ const Register = () => {
 
 
   return (
-    <div>
-      <h2>Register</h2>
-      <form >
-        <input
-          type="userName"
-          placeholder="UserName"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <input
-          type="name"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="surname"
-          placeholder="Surname"
-          value={surname}
-          onChange={(e) => setSurname(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <select value={gender} onChange={(e) => setGender(e.target.value)}>
-          <option value="">Select Gender</option>
-          <option value="Female">Female</option>
-          <option value="Male">Male</option>
-          <option value="Other">Other</option>
-        </select>
-        <select value={userType} onChange={(e) => setRole(e.target.value)}>
-          <option value="">Select Role</option>
-          <option value="student">Student</option>
-          <option value="teacher">Teacher</option>
-          <option value="admin">Admin</option>
-        </select>
-        <button type="submit" onClick={()=>RegisterSubmit(userName,name,surname,email,password,gender,userType)}>Register</button>
-      </form>
+
+    <div className='main'>
+      <Container maxWidth="lg">
+        <Header/>
+        <Grid container justifyContent="center">
+          <Grid sx={{paddingLeft:2}}>
+            <div>
+              <form >
+                <FormControl sx={{width:'50ch'}}>
+                  <TextField variant='filled' id="outlined-required" label="Username" type="userName"  value={userName} onChange={(e) => setUserName(e.target.value)} sx={{margin:2, backgroundColor:'white'}}/>
+                  <TextField variant='filled' id="outlined-required" label="Name" type="name" value={name} onChange={(e) => setName(e.target.value)} sx={{margin:2, backgroundColor:'white'}}/>
+                  <TextField variant='filled' id="outlined-required" label="Surname"  type="surname" value={surname} onChange={(e) => setSurname(e.target.value)} sx={{margin:2, backgroundColor:'white'}}/>
+                  <TextField variant='filled' id="outlined-required" label="Email"  type="email" value={email} onChange={(e) => setEmail(e.target.value)} sx={{margin:2, backgroundColor:'white'}}/>
+                  <TextField variant='filled' id="outlined-required" label="Password"  type="password" value={password} onChange={(e) => setPassword(e.target.value)} sx={{margin:2, backgroundColor:'white'}}/>
+                  <FormControl variant="filled" sx={{ m: 1, width:'50ch' }}>
+                    <InputLabel sx={{margin:1}}>Gender</InputLabel>
+                    <Select value={gender} onChange={(e) => setGender(e.target.value)} sx={{margin:1,marginRight:3, backgroundColor:'white'}}>
+                      <MenuItem value={'Female'}>Female</MenuItem>
+                      <MenuItem value={'Male'}>Male</MenuItem>
+                      <MenuItem value={'Other'}>Other</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <FormControl variant="filled" sx={{ m: 1, width:'50ch' }}>
+                    <InputLabel  sx={{margin:1}}>User Type</InputLabel>
+                    <Select  value={userType} onChange={(e) => setRole(e.target.value)} sx={{margin:1,marginRight:3, backgroundColor:'white'}}>
+                      <MenuItem value={'student'}>Student</MenuItem>
+                      <MenuItem value={'teacher'}>Teacher</MenuItem>
+                      <MenuItem value={'admin'}>Admin</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <Box sx={{display: 'flex', justifyContent: 'space-between', width: '100%',}}>
+                    <Button variant="contained" href='/login' >
+                        Login
+                    </Button>
+                    <Button type="submit" variant="contained" color='success' onClick={()=>RegisterSubmit(userName,name,surname,email,password,gender,userType)}>
+                        Register
+                    </Button>
+                </Box>
+                </FormControl>
+              </form>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
