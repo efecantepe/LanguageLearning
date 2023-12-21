c5d4d35456625274703c27370e770297f341ddd6
Alperen Erdoğan <alperenerdogan@yahoo.com>
2023-12-21 23:21:30 +0300

    people list

--- learning-language/src/Pages/Chat.js
+++ learning-language/src/Pages/Chat.js
@@ -7,15 +7,24 @@ import ChatCenterPanel from '../Components/ChatCenterPanel';
 const previousChats = [
   { id: 1, name: 'Eylul', lastMessage: 'Last message in Chat 1' },
   { id: 2, name: 'Alperen', lastMessage: 'Last message in Chat 2' },
+  { id: 3, name: 'Efe', lastMessage: 'Last message in Chat 2' },
 ];
 
-const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+const peopleList = [
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Jane Smith' },
+  { id: 3, name: 'Michael Johnson' },
+];
+
+const getColorForLetter = (letter) => {
+  const colorMap = {
+    A: '#FFD1DC', B: '#FFD1B3', C: '#FFE6B3', D: '#FFF5B3', E: '#F3FFB3', F: '#D1FFB3',
+    G: '#B3FFC4', H: '#B3FFE6', I: '#B3FFFF', J: '#B3EDFF', K: '#B3D1FF', L: '#CBB3FF',
+    M: '#E6B3FF', N: '#FFB3FF', O: '#FFB3E6', P: '#FFB3D1', Q: '#FFCBB3', R: '#FFE6B3',
+    S: '#FFFFB3', T: '#EDFFB3', U: '#D1FFB3', V: '#B3FFCB', W: '#B3FFE6', X: '#B3FFFF',
+    Y: '#B3EDFF', Z: '#B3D1FF',
+  };
+  return colorMap[letter.toUpperCase()] || '#CCCCCC'; // Default color if letter not found
 };
 
 const LeftPanel = () => {
@@ -28,8 +37,8 @@ const LeftPanel = () => {
         {previousChats.map((chat) => (
           <React.Fragment key={chat.id}>
             <ListItem button>
-              <Avatar sx={{ bgcolor: getRandomColor() }}> {chat.name.charAt(0)} </Avatar>
-              <ListItemText primary={chat.name} secondary={chat.lastMessage} sx={{paddingLeft:2}}/>
+              <Avatar sx={{color:'MintCream', bgcolor: getColorForLetter(chat.name.charAt(0)) }}> {chat.name.charAt(0)} </Avatar>
+              <ListItemText primary={chat.name} secondary={chat.lastMessage} sx={{ paddingLeft: 2 }} />
             </ListItem>
             <Divider />
           </React.Fragment>
@@ -52,6 +61,17 @@ const RightPanel = () => {
   return (
     <div className="right-panel">
       <h2>People List</h2>
+      <List>
+        {peopleList.map((person) => (
+          <React.Fragment key={person.id}>
+          <ListItem button>
+          <Avatar sx={{color:'MintCream', bgcolor: getColorForLetter(person.name.charAt(0)) }}> {person.name.charAt(0)} </Avatar>
+            <ListItemText primary={person.name} sx={{ paddingLeft: 2 }} />
+          </ListItem>
+          <Divider />
+        </React.Fragment>
+        ))}
+      </List>
     </div>
   );
 };
